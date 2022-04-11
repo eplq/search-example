20f2e77b6adaa70015571b33316551e7c3d6faf6
gato <91148468+eplq@users.noreply.github.com>
2022-04-11 11:53:04 +0200

    ya podríamos empezar a implementar

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("search example")
 
-
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 app.run("0.0.0.0", 8101, True)
