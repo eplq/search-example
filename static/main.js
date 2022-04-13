7ea54d04666c60547182dbc8fb22c8ee575da431
eplq <91148468+eplq@users.noreply.github.com>
2022-04-13 09:53:14 +0200

    obtención de resultados y búsqueda normal

--- static/main.js
+++ static/main.js
@@ -1,5 +1,6 @@
 const selectElement = document.querySelector("select");
 const tableHead = document.querySelector("thead");
+const tableBody = document.querySelector("tbody");
 
 fetch("/fields")
     .then(res => res.json())
@@ -18,3 +19,45 @@ fetch("/fields")
             tableHead.append(column);
         }); 
     });
+
+function fetchQuery(ev) {
+    const query = ev.target.value;
+
+    fetch(`/search?field=${selectElement.value}&query=${query}`)
+    .then(res => res.json())
+    .then(data => {
+        tableBody.innerHTML = "";
+
+        data.forEach(item => {
+            const tableRow = document.createElement("tr");
+
+            item.forEach(cell => {
+                const tableCell = document.createElement("td");
+                tableCell.textContent = cell;
+
+                tableRow.append(tableCell);
+            });
+
+            tableBody.append(tableRow);
+        });
+    });
+}
+
+
+let normalCounter = 0;
+let throttleCounter = 0;
+let debounceCounter = 0;
+
+const normalRequests = document.querySelector("#normalRequests");
+const throttleRequests = document.querySelector("#throttleRequests");
+const debounceRequests = document.querySelector("#debounceRequests");
+
+const normalInput = document.querySelector("#normal");
+const throttleInput = document.querySelector("#throttle");
+const debounceInput = document.querySelector("#debounce");
+
+normalInput.addEventListener("input", ev => {
+    normalCounter++;
+    normalRequests.textContent = normalCounter;
+    fetchQuery(ev);
+});
