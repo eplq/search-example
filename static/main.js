3ebffcdcdd7109079452c78f74b400d9c77c6a25
eplq <91148468+eplq@users.noreply.github.com>
2022-04-14 09:48:36 +0200

    throttle

--- static/main.js
+++ static/main.js
@@ -94,12 +94,12 @@ function debounce(cb, delay=250) {
     return (...args) => {
         clearTimeout(timeout);
         timeout = setTimeout(() => {
-            cb(...args)
+            cb(...args);
         }, delay);
     }
 }
 
-/**
+/*
  * ¿Por qué no lo podemos poner así? Porque no se mantiene la variable timeout entre
  * llamadas, lo que hace que solo se retrase cada una de las llamadas al callback, con
  * lo que estás consiguiendo lo mismo que haciéndolo directamente, pero retrasándolo.
@@ -114,3 +114,61 @@ function debounce(cb, delay=250) {
  */
 const debounceFetchQuery = debounce(fetchQuery, 500);
 debounceInput.addEventListener("input", debounceFetchQuery);
+
+
+function throttle(cb, delay=1000) {
+    let shouldWait = false;
+    let waitingArgs; // La idea es retener los argumentos para la siguiente llamada al callback
+
+    /*
+     * Cuando no hay argumentos en espera, significa que
+     * que ya no debe de esperar y se sale. Esto es porque
+     * ya se ha llamado al callback y no hay más eventos.
+     * 
+     * Cuando hay argumentos en espera, ejecutamos el callback
+     * y anulamos los argumentos con los que acabamos de ejecutar
+     * el callback, porque ya no son válidos para la siguiente
+     * llamada.
+     * 
+     * Volvemos a programar la ejecución de la función de debajo,
+     * si ha habido un nuevo evento, habrá argumentos por lo que se
+     * ejecutará el callback tras el delay. Si no lo ha habido, los
+     * argumentos en espera seguirán siendo nulos y por tanto ya no
+     * se ejecutará más hasta que vuelva a haber eventos.
+     */
+    const timeoutFunction = () => {
+        if (waitingArgs == null) {
+            shouldWait = false;
+        } else {
+            cb(...waitingArgs);
+            waitingArgs = null;
+            setTimeout(timeoutFunction, delay);
+        }
+    }
+
+    return (...args) => {
+
+        /*
+         * Si ya está esperando,
+         * actualizamos los argumentos que se pasarán al
+         * callback para tener la información más actualizada.
+         */
+        if (shouldWait) {
+            waitingArgs = args;
+            return;
+        }
+
+        /*
+         * Cuando ya no hay que esperar, llamamos al callback
+         * y establecemos que hay que volver a esperar. Tras ello,
+         * activamos el timeout, que hará que se ejecute la función
+         * de más arriba.
+         */
+        cb(...args);
+        shouldWait = true;
+        setTimeout(timeoutFunction, delay);
+    }
+}
+
+const throttleFetchQuery = throttle(fetchQuery, 500);
+throttleInput.addEventListener("input", throttleFetchQuery);
